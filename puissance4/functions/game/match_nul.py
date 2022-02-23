03e406d9f200ff98c415265b3072cb30bafee191
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-23 19:03:36 +0100

    Create match_nul.py

---
+++ puissance4/functions/game/match_nul.py
@@ -0,0 +1,7 @@
+def match_nul(grid: list):
+    '''
+    Check on the whole grid if there are movements
+    Arg: grid [grid] (Array of array of int)
+    Out: Are there movements ? (bool)
+    '''
+    return True if 0 in grid[len(grid) - 1] else False
